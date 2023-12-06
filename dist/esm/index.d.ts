89bf420c7ea63f08c435ca123afcd1a3a25fe64a
simonkravec <simonko420@gmail.com>
2023-12-06 20:43:33 +0100

    add dist

---
+++ dist/esm/index.d.ts
@@ -0,0 +1,4 @@
+import type { CapacitorFirebaseDynamicLinksPlugin } from './definitions';
+declare const FirebaseDynamicLinks: CapacitorFirebaseDynamicLinksPlugin;
+export * from './definitions';
+export { FirebaseDynamicLinks };
